2f79111eb239b48d8fc94fcab94f03cf1a3093d3
rizkygin <rizkygin@gmail.com>
2019-03-21 17:54:10 +0700

    21

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Dashboard</div>
+                <div class="card-header">Informasi</div>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -13,11 +13,58 @@
                             {{ session('status') }}
                         </div>
                     @endif
+                      <div class="table-responsive">
+                        <table class="table">
+                          <form class="" action="/absen" method="post">
+                            {{csrf_field()}}
+                            <tr>
+                              <td>
+                                <input class="from-cotrol" type="text" name="note" value="">
+                              </td>
+                              <td>
+                                <button class="btn btn-flat btn-primary" type="text" name="btnIN" value="">ABSEN MASUK</button>
+                              </td>
+                              <td>
+                                <button class="btn btn-flat btn-primary" type="text" name="btnOUT" value="">ABSEN KELUAR</button>
+                              </td>
+                            </tr>
+                          </form>
+                        </table>
+                      </div>
 
-                    You are logged in!
                 </div>
             </div>
         </div>
-    </div>
+      </div>
+      <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Riwayat Absensi</div>
+
+                    <div class="card-body">
+                      <div class="table-responsive table-bordered table-hover">
+                        <table class="table">
+                          <thead>
+                            <tr>
+                              <th>Tanggal</th>
+                              <th>Jam Masuk</th>
+                              <th>Jam Keluar</th>
+                              <th>Keterangan</th>
+                            </tr>
+                          </thead>
+                          <tbody>
+                            <tr>
+                              <td>2016-01-03</td>
+                              <td>02:00:00</td>
+                              <td>02:00:00</td>
+                              <td>Hello</td>
+                            </tr>
+                          </tbody>
+                        </table>
+                      </div>
+                    </div>
+                </div>
+            </div>
+        </div>
 </div>
 @endsection
